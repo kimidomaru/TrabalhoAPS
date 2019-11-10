87543a4e49f70da7919aace2db5d5e2eeef04eaf
caioseda <caioseda@hotmail.com>
2019-11-10 01:04:57 -0300

    Print Classe/Interface padrao UML

--- Composite/src/composite/Menu.java
+++ Composite/src/composite/Menu.java
@@ -1,5 +1,6 @@
 package composite;
 
+import java.util.ArrayList;
 
 public class Menu {
 	public void mostrarMenu(String elemento) {
@@ -87,5 +88,96 @@ public class Menu {
 	
 	public void menuRelacionamento(){
 		//opcoes Aqui
+	}
+	public void ShowComponente(ComponenteComposto obj) {
+		String nome = obj.getNome();
+		String type= "";
+		
+		ArrayList filhos = (ArrayList) obj.getFilhos();
+		ArrayList<String> atributos = new ArrayList();
+		ArrayList<String> metodos = new ArrayList();
+		
+		if(obj instanceof Classe) {
+			
+			type = "Classe";
+		}
+		if(obj instanceof Interface) {
+		
+			type = "Interface";
+		}
+		String Header="|          "+ type+ " " +obj.getNome()+ "          |";
+		
+		int TamHeader = Header.length(); 
+		String dashes = new String(new char[TamHeader-2]).replace("\0", "-");
+		
+		for(int i=0;i < filhos.size();i++) {
+			
+			String typeFilho = "";
+			String Conteudo = "";
+			
+			if (filhos.get(i) instanceof Atributo) {
+				
+				Atributo davez =(Atributo) filhos.get(i); 
+				//System.out.println("Atributo!");
+				String ModificadorAcesso = "";	
+				
+				switch(davez.getModificadoresDeAcesso().toLowerCase()) {
+					case "public":
+						ModificadorAcesso="+";
+					case "private":
+						ModificadorAcesso="-";
+					case "protected":
+						ModificadorAcesso="#";
+					case "default":
+						ModificadorAcesso="~";
+					default:
+						ModificadorAcesso="~";
+				}
+				
+				String preConteudo="| "+ ModificadorAcesso + davez.getNome()+ " : "+ davez.getTipo();
+				int tamPreConteudo = preConteudo.length();
+				String spaces = new String(new char[TamHeader-tamPreConteudo-1]).replace("\0", " ");
+				Conteudo = preConteudo + spaces+"|";
+						
+				atributos.add(Conteudo);
+			}
+			if (filhos.get(i) instanceof Metodo) {
+				Metodo davez =(Metodo) filhos.get(i);
+				//System.out.println("Metodo!");
+				String ModificadorAcesso = "";		
+				
+				switch(davez.getModificadoresDeAcesso().toLowerCase()) {
+					case "public":
+						ModificadorAcesso="+";
+					case "private":
+						ModificadorAcesso="-";
+					case "protected":
+						ModificadorAcesso="#";
+					case "default":
+						ModificadorAcesso="~";
+					default:
+						ModificadorAcesso="+";
+				}
+				
+				String preConteudo="| "+ ModificadorAcesso + davez.getNome()+ "():"+ davez.getTipoRetorno();
+				int tamPreConteudo = preConteudo.length();
+				String spaces = new String(new char[TamHeader-tamPreConteudo-1]).replace("\0", " ");
+				Conteudo = preConteudo + spaces+"|";
+				
+				metodos.add(Conteudo);
+			}
+		}
+		
+		System.out.println("\n+"+ dashes +"+"); // 25 -  // 2 +
+ 		System.out.println(Header);
+ 		System.out.println("+"+ dashes +"+");
+ 		for(int i=0;i<atributos.size();i++) {
+ 			System.out.println(atributos.get(i));
+ 		}
+ 		System.out.println("+"+ dashes +"+");
+ 		for(int i=0;i<metodos.size();i++) {
+ 			System.out.println(metodos.get(i));
+ 		}
+ 		System.out.println("+"+ dashes +"+");
 	}
 }
